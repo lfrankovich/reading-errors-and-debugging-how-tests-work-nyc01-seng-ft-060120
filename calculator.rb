86a8aed645835203978fe65dfd8233d459b94d95
Lauren Frankovich <frankovister@gmail.com>
2020-05-22 03:01:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,21 +4,3 @@ first_number = 4
 second_number = 2 
 sum = first_number + second_number
 difference = first_number + second_number
-
-
-  it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    product = get_variable_from_file('./calculator.rb', "product")
-
-    expect(product).to eq(first_number*second_number)
-  end
-
-  it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    quotient = get_variable_from_file('./calculator.rb', "quotient")
-
-    expect(quotient).to eq(first_number/second_number)
-  end
-end
